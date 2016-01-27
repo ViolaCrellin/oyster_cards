4b4f8c55ad27d26da9ef260d4b34521360f7bdc5
ViolaCrellin <viola.crellin@gmail.com>
2016-01-27 11:53:09 +0000

    Failing tests for 'Completed?'

--- lib/journey.rb
+++ lib/journey.rb
@@ -2,13 +2,24 @@
 
 class Journey
 
-  attr_reader :this_journey
+  attr_reader :this_journey, :exit_station, :entry_station
 
   def initialize
     @this_journey = {}
   end
 
-  def in_journey?
+  def start_journey(entry_station)
+    @entry_station = entry_station
+  end
+
+
+  def end_journey(exit_station)
+    @exit_station = exit_station
+  end
+
+
+  def completed?
+
   end
 
 end
--- spec/journey_spec.rb
+++ spec/journey_spec.rb
@@ -3,33 +3,57 @@ require 'journey'
 describe Journey do
 
   subject(:journey) {described_class.new}
+  let(:station1) {double :station}
+  let(:station2) {double :station}
 
-  it {is_expected.to respond_to(:in_journey?)}
-  it {is_expected.to respond_to(:start_journey)}
-  it {is_expected.to respond_to(:end_journey)}
+  describe "#initialize" do
 
-  it 'has an empty hash to save current journey' do
-    expect(journey.this_journey).to eq ({})
+    it 'has an empty hash to save current journey' do
+      expect(journey.this_journey).to eq ({})
+    end
+
   end
 
-  context 'keeping track of journey state' do
+  describe "#start_journey" do
 
     before do
-      let(:station1) {double :station}
-      let(:station2) {double :station}
-      journey.start_journey station
+      journey.start_journey station1
       journey.end_journey station2
     end
+
+    it {is_expected.to respond_to(:start_journey).with(1).argument}
 
     it 'is able to store and retrieve starting station' do
       expect(journey.entry_station).to eq station1
     end
 
+
+  describe "#end_journey" do
+
+    it {is_expected.to respond_to(:end_journey).with(1).argument}
+
     it 'is able to store and retrieve exit station' do
-      expect(journey.exit_station).to eq station2
+        expect(journey.exit_station).to eq station2
+    end
+
+  end
+end
+
+  describe "#completed?" do
+
+    it {is_expected.to respond_to(:completed?)}
+
+    before {journey.start_journey station1}
+
+    it "returns not completed when either exit station or entry station is nil" do
+      expect(journey.completed?).to be false
     end
 
 
+    it "returns true when there is an exit station and an exit station" do
+      journey.end_journey station2
+      expect(journey.completed?).to be true
+    end
+    
   end
-
 end
